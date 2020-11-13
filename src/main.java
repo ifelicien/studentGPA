42714643e1978e8074f29a326e8633f315dc7736
irwin felicien <ifelicien@gmail.com>
2020-11-13 11:29:49 -0600

    added array to loop prints list of 6 names

--- src/main.java
+++ src/main.java
@@ -1,26 +1,28 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+
 
 public class main {
     public static void main(String[] args) {
-        String sName = nameFinder();
-        display(sName)
-        ;
-    }
+        ArrayList<String> studentNames = new ArrayList<>();
+        int[] count = {1,2,3,4,5,6};
 
-    static String nameFinder(){
+
         Scanner myScanner = new Scanner(System.in);
         System.out.println("Please enter the students name: ");
 
         for(int i = 0;i < 6;i++) {
-            String studentName = myScanner.nextLine();
-
-            return studentName;
+            String studentName = myScanner.next();
+            studentNames.add(studentName);
         }
-        return null;
-    }
 
-    static void display(String name){
-        System.out.println(name);
+        System.out.println("\n\n" + count[0] + ". " + studentNames.get(0));
+        System.out.println(count[1] + ". " + studentNames.get(1));
+        System.out.println(count[2] + ". " + studentNames.get(2));
+        System.out.println(count[3] + ". " + studentNames.get(3));
+        System.out.println(count[4] + ". " + studentNames.get(4));
+        System.out.println(count[5] + ". " + studentNames.get(5));
     }
 }
 
